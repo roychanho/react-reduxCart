bcb9cab783c76d68ceb26bd5748bf36f192a6b49
CHAN Ho Long <roychanho@gmail.com>
2023-02-02 13:01:41 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cartActions";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    // thunk function
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    //first time block
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    // thunk function
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
+  );
+}
+
+export default App;
